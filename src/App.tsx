8e22d0fef42b72b5bdb3a92da66aa56bf5f9ec08
Ruben Mateus <ruben.mateus3@gmail.com>
2025-05-07 18:46:26 +0100

    exercise-1-solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+
 export const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
