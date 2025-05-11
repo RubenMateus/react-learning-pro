e678629532ae09296df7617cb0960f41f13e1858
Ruben Mateus <ruben.mateus3@gmail.com>
2025-05-11 21:07:54 +0100

    exercise 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import "./App.css";
 
 export const App = () => {
   return (
     <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">Início</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">Sobre</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contacto</NavLink>
+          </li>
+        </ul>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
